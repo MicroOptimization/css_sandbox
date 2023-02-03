ffb18c2eefbde7cb176df1935164610a2dc5b3ac
MicroOptimization <jackiezhen538@gmail.com>
2023-02-03 16:21:23 -0500

    interpreted some more of this ridiculously dense tutorial

--- hacker_complete/scripts.js
+++ hacker_complete/scripts.js
@@ -1,16 +1,21 @@
 
 function hacker_function(id) {
-
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+    
     let interval = null;
+    console.log("thing: " + event)
     document.querySelector("#" + id).onmouseover = event => {  
         let iteration = 0;
         clearInterval(interval);
+        console.log("here")
+        //console.log("hi: " + event)
+        
+        //Runs the code inside the anonymous function indicated by => every x (miliseconds I assume) 30 in this case is x presumably.
+        /*
         interval = setInterval(() => {
-            alert(typeof event.target)
             event.target.innerText = event.target.innerText
             .split("")
+            
             .map((letter, index) => {
                 if(index < iteration) {
                 return event.target.dataset.value[index];
@@ -21,10 +26,30 @@ function hacker_function(id) {
             .join("");
             
             if(iteration >= event.target.dataset.value.length) { 
-                clearInterval(interval);
+                clearInterval(interval);//Stops the interval from running
             }
             
             iteration += 1 / 3;
-        }, 30);
+        }, 30);*/
+
+        //Essentially the above function is this, which is objectively less clean, but more palettable to my eyes
+        //I've written so much python that it's weird when you can do your own styling conventions in other languages
+        //Also there are like 2 nested anonymous functions in here :/
+
+        console.log(event.target)
+        interval = setInterval(() => { //Curly boi here
+            event.target.innerText = event.target.innerText.split("").map((letter, index) => { //Curly boi here
+                if(index < iteration) {
+                    return event.target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)]
+            }).join(""); //Curly boi here
+            
+            if(iteration >= event.target.dataset.value.length) {  //
+                clearInterval(interval); //Stops the interval from running
+            }
+            
+            iteration += 1 / 3;
+        }, 30); //Curly boi here
     }
 }
